0d66e2d4446247dbf82de978918a94dda5a5fcf2
Abel <abelsena1@hotmail.com>
2020-06-29 17:29:53 -0300

    Connection with the database

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const db = require('./models/index')
 
 const authController = require('./controllers/auth')
 
@@ -10,7 +11,9 @@ app.get('/', (req, res) => {
     return res.json('Api is Running!')
 })
 
+db.sequelize.sync().then(() => {
+ 
 app.listen(3001, () => {
     console.log('Server running on port 3001')
 })
-
+})
